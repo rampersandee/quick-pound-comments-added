d8b76dc46274a2e1ca66e9ed3dc72eae084ba9a0
Brandon Mullings (Randy) <126500708+rampersandee@users.noreply.github.com>
2023-05-13 13:43:56 -0400

    Quickpound Update
    Added comment functionality to user posts.

---
+++ models/Comments.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const CommentsSchema = new mongoose.Schema({
+  comment: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Post",
+  },
+  likes: {
+    type: Number,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Comments", CommentsSchema);
